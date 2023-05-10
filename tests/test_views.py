6abab30f23bf7d8df35f1fde661254a9aa341774
ivan4oto <redial@abv.bg>
2023-05-10 20:44:41 +0300

    Useless config prop

--- tests/test_views.py
+++ tests/test_views.py
@@ -6,7 +6,6 @@ class TestViews(unittest.TestCase):
 
     def setUp(self):
         app.config['TESTING'] = True
-        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
         self.client = app.test_client()
         with app.app_context():
             db.create_all()
