8ff9977baedfaea1b79c3ed4fa77dad1bf5cdf34
ivan4oto <redial@abv.bg>
2023-05-10 16:03:14 +0300

    Get Last Order Per Customer endpoint

--- app/queries.py
+++ app/queries.py
@@ -14,3 +14,11 @@ GROUP BY p.product_name
 ORDER BY total_quantity DESC
 LIMIT 10;
 """
+
+get_last_order_per_customer = """
+SELECT c.customer_id, c.customer_email, MAX(t.transaction_date) as last_order_date
+FROM customer c
+JOIN transaction t ON c.id = t.customer_id
+GROUP BY c.customer_id, c.customer_email
+ORDER BY c.customer_id;
+"""
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from flask import jsonify
 from sqlalchemy import text
 from app import app, db
-from app.queries import get_customers_with_birthday_today, get_top_products_for_year
+from app.queries import get_customers_with_birthday_today, get_top_products_for_year, get_last_order_per_customer
 
 @app.route('/customers/birthday', methods=['GET'])
 def birthday_customers():
@@ -10,6 +10,19 @@ def birthday_customers():
         results = [{'customer_id': row[0], 'customer_first_name': row[1]} for row in results]
     return jsonify(customers=results)
 
+@app.route('/customers/last-order-per-customer', methods=['GET'])
+def last_order_per_customer():
+    with db.engine.connect() as conn:
+        results = conn.execute(text(get_last_order_per_customer))
+        customers = [
+            {
+                "customer_id": row[0],
+                "customer_email": row[1],
+                "last_order_date": row[2].strftime("%Y-%m-%d") if row[2] else None,
+            }
+            for row in results
+        ]
+    return jsonify(customers=customers)
 
 @app.route('/products/top-selling-products/<year>', methods=['GET'])
 def top_selling_products(year):
